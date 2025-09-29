fd74ecf654f28594c7c6340a48f88cfe04dfcba2
cirovence <ciro.vence@prosperidadsocial.gov.co>
2025-09-28 23:18:06 -0500

    Primer commit proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // habilita *ngIf, *ngFor, *ngSwitch
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'formulario-app';
+
+  // pestaña activa por defecto
+  activeTab: string = 'estudiantes';
+
+  // cambiar pestaña
+  setActiveTab(tab: string) {
+    this.activeTab = tab;
+  }
+
+  // 🔹 Datos simulados (mock)
+  estudiantes = [
+    { nombre: 'Juan Pérez', correo: 'juan.perez@ucc.edu.co', programa: 'Ingeniería de Software' },
+    { nombre: 'María López', correo: 'maria.lopez@ucc.edu.co', programa: 'Medicina' }
+  ];
+
+  libros = [
+    { titulo: 'Cien años de soledad', autor: 'Gabriel García Márquez' },
+    { titulo: 'El Quijote', autor: 'Miguel de Cervantes' },
+    { titulo: 'La Odisea', autor: 'Homero' }
+  ];
+
+  prestamos = [
+    { estudiante: 'Juan Pérez', libro: 'Cien años de soledad', fecha: '2025-09-25' }
+  ];
 }
